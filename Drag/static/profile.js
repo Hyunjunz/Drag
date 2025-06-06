1c7d716a3d632a8a7acba2096fd83523eb1a7255
Hyeonjun <113574910+Hyunjunz@users.noreply.github.com>
2025-06-06 22:26:08 +0900

    Add files via upload

---
+++ Drag/static/profile.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+const modal = document.getElementById('myModal');
+const openBtn = document.getElementById('openModalBtn');
+const closeBtn = document.getElementById('closeModalBtn');
+
+openBtn.addEventListener('click', () => {
+    modal.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+    modal.style.display = 'none';
+    }
+});
+});
